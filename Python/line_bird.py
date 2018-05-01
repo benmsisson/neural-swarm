a72c055ca6c3ef6ee8393acac09bc811d989be30
Benjamin Sisson <benmsisson@gmail.com>
2018-05-01 16:25:18 -0400

    Deled Python files

--- Python/line_bird.py
+++
@@ -1,9 +0,0 @@
-from base_bird import *
-
-class LineBird(BaseBird):
-    def needs_grid(self):
-        return False
-        
-    def make_decision(self,bird_number):
-        return aim_at_position(self.ws.birds[bird_number],self.ws.goal_pos)
-
